a25494eeab526c20fed1a159dac22e2103aa627b
Zodiacal0 <javierherrera5513@gmail.com>
2025-08-08 21:52:04 -0600

    feat(middlewares): implementacion de task validators

---
+++ src/middlewares/task-validator.js
@@ -0,0 +1,34 @@
+import { body, param } from "express-validator";
+import { validationsFields } from "./fields-validator.js";
+import { catchErrors } from "./catch-errors.js";
+
+export const createTaskValidator = [
+    body("title").notEmpty().withMessage("El título es obligatorio"),
+    body("description").notEmpty().withMessage("La descripción es obligatoria"),
+    body("dueDate").isDate().withMessage("La fecha de vencimiento no es válida"),
+    body("status").isIn(["PENDIENTE", "PROGRESO", "COMPLETADA"]).withMessage("El estado no es válido"),
+    validationsFields,
+    catchErrors
+];
+
+export const getTasksByStatusValidator = [
+    body("status").isIn(["PENDIENTE", "PROGRESO", "COMPLETADA"]).withMessage("El estado no es válido"),
+    validationsFields,
+    catchErrors
+];
+
+export const updateTaskValidator = [
+    param("uid").isMongoId().withMessage("El ID de la tarea no es válido"),
+    body("title").notEmpty().withMessage("El título es obligatorio"),
+    body("description").notEmpty().withMessage("La descripción es obligatoria"),
+    body("dueDate").isDate().withMessage("La fecha de vencimiento no es válida"),
+    body("status").isIn(["PENDIENTE", "PROGRESO", "COMPLETADA"]).withMessage("El estado no es válido"),
+    validationsFields,
+    catchErrors
+]
+
+export const deleteTaskValidator = [
+    param("uid").isMongoId().withMessage("El ID de la tarea no es válido"),
+    validationsFields,
+    catchErrors
+];
